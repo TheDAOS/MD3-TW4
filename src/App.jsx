580ef00232c5c20b16a0c7956738ae139529be6b
theDAOS <sanjusanthosh46@gmail.com>
2025-05-19 23:51:30 +0530

    refactor: remove Grommet integration and unused styles from App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,15 @@
 import { useState } from 'react';
 import MarkdownEditor from './components/MarkdownEditor'
-import { Grommet, Grid, Box } from 'grommet';
 import ReactMarkdown from 'react-markdown';
 
 function App() {
   const [markdown, setMarkdown] = useState('');
 
-  const theme = {
-    global: {
-      colors: {
-        bg: '#000000',
-      },
-      font: {
-        family: "Roboto",
-        size: "18px",
-        height: "20px",
-      },
-    },
-  };
-
   return (
-    <Grommet full theme={theme} background='bg'>
-      <Grid
-        rows={['xxsmall', 'xsmall']}
-        columns={['xsmall', 'small']}
-        gap="small"
-      >
-        <Box><MarkdownEditor markdown={markdown} setMarkdown={setMarkdown} /></Box>
-        <Box background="light-5" ><ReactMarkdown>{markdown}</ReactMarkdown></Box>
-      </Grid>
-
-    </Grommet>
+    <>
+      <MarkdownEditor markdown={markdown} setMarkdown={setMarkdown} />
+      <ReactMarkdown>{markdown}</ReactMarkdown>
+    </>
   )
 }
 

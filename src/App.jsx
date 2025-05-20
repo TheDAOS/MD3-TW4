f785d11da3bb565aab1113e9f4414d125a01b38f
theDAOS <sanjusanthosh46@gmail.com>
2025-05-20 08:01:27 +0530

    refactor: replace Stack with Grid layout for improved responsiveness in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { useState } from 'react';
 import MarkdownEditor from './components/MarkdownEditor'
 import ReactMarkdown from 'react-markdown';
-import { Stack } from '@mui/material';
+import { Grid, Stack } from '@mui/material';
 import './App.css'
 
 function App() {
   const [markdown, setMarkdown] = useState('');
 
   return (
-    <Stack direction="row" spacing={2} justifyContent='space-between'>
-      <MarkdownEditor markdown={markdown} setMarkdown={setMarkdown} />
-      <ReactMarkdown>{markdown}</ReactMarkdown>
-    </Stack>
+    <Grid container spacing={2}>
+      <Grid size={6}>
+        <MarkdownEditor markdown={markdown} setMarkdown={setMarkdown} />
+      </Grid>
+      <Grid size={6}>
+        <ReactMarkdown>{markdown}</ReactMarkdown>
+      </Grid>
+    </Grid>
   )
 }
 

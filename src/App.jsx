3ee4b555005fae5e68ebde2b888c639b3684d2c1
theDAOS <sanjusanthosh46@gmail.com>
2025-05-20 08:25:58 +0530

    refactor: replace MarkdownEditor component with TextareaAutosize for simplified markdown input

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { useState } from 'react';
-import MarkdownEditor from './components/MarkdownEditor'
 import ReactMarkdown from 'react-markdown';
-import { Grid, Stack } from '@mui/material';
+import { Grid } from '@mui/material';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
 import './App.css'
 
 function App() {
   const [markdown, setMarkdown] = useState('');
 
+  const handleChange = (e) => {
+    setMarkdown(e.target.value);
+  };
+
   return (
     <Grid container spacing={2}>
       <Grid size={6}>
-        <MarkdownEditor markdown={markdown} setMarkdown={setMarkdown} />
+        <TextareaAutosize
+          value={markdown}
+          onChange={handleChange}
+          placeholder="Write your Markdown here..."
+        />
       </Grid>
       <Grid size={6}>
         <ReactMarkdown>{markdown}</ReactMarkdown>
